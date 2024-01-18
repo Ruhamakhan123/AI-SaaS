2fdc148f4292d8ca65a17c93eb24d308bf2562af
Ruhamakhan123 <105592893+Ruhamakhan123@users.noreply.github.com>
2024-01-18 21:15:02 +0500

    change

--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -30,7 +30,7 @@ const LandingHero = () => {
         Create content using AI 10x faster.
       </div>
       <div>
-        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+        <Link href={isSignedIn ? "/dashboard" : "/sign-in"}>
           <Button
             variant="premium"
             className="md:text-lg p-4 md:p-6 rounded-full font-semibold"
